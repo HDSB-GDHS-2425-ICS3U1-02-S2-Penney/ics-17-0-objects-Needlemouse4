87af35a581689e105839e5a59f06a912b92dda7e
Needlemouse4 <1dykstrataz@hdsb.ca>
2025-05-23 13:35:53 -0400

    Finished Assigment, and added random colour, transparency and size :)

--- script.js
+++ script.js
@@ -15,23 +15,46 @@ function init() {
 /**** OBJECT CREATION FUNCTIONS ****/
 
 // Write the circle object function here
-
+function circle(x, y, color, size) {
+  this.x = x;
+  this.y = y;
+  this.color = color;
+  this.size = size; 
+}
 
 
 // Write the createCircle function here. 
+const objs = [];
+let currentcircle = 0;
 
+function createCircle() {
 
+  objs[currentcircle] = new circle(randomInteger(0, 1500), randomInteger(0, 1000), randomColor(), randomInteger(0, 50));
+
+  ctx.beginPath();
+  ctx.arc(objs[currentcircle].x, objs[currentcircle].y, objs[currentcircle].size, 0, Math.PI * 2);
+  ctx.fillStyle = objs[currentcircle].color;
+  ctx.fill();
+}
 
 // Write the randomInteger function here. 
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
 
-
-
+function randomColor() {
+  let r = Math.round(Math.random() * 256);
+  let g = Math.round(Math.random() * 256);
+  let b = Math.round(Math.random() * 256);
+  let a = Math.random();
+  return "rgba("+r+","+g+","+b+", "+a+")";
+}
 /**** GAMELOOP ****/
 
 function gameLoop(timestamp) {
 
   // Call the createCircle function here.
-
+  createCircle();
 
   // This causes the game to loop every frame (Do not change this)
   window.requestAnimationFrame(gameLoop);
